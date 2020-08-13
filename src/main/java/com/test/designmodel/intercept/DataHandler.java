f2ac56160e5d669522fdfad35c26117e1751ab8b
Remous <1605649124@qq.com>
2020-08-13 22:47:41 +0800

    提交注解类

--- src/main/java/com/test/designmodel/intercept/DataHandler.java
+++ src/main/java/com/test/designmodel/intercept/DataHandler.java
@@ -1,7 +1,40 @@
 package com.test.designmodel.intercept;
 
 
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface  DataHandler {
+    /**
+     * bean的全类名
+     * */
+    String bean() default "";
 
+    /**
+     * 执行bean的方法
+     * */
+    String method () default "";
 
+    /**
+     * 执行方法的入参
+     * */
+    String params() default "";
+
+    /**
+     * 执行方法的入参类型
+     * */
+    String paramType () default "";
+
+    /**
+     * 执行方法后的返回类型
+     * */
+    String returnType() default "";
+
+    /**
+     * 标题
+     * */
+    String titile() default "";
 }
